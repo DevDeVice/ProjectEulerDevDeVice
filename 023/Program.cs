09d796db7b8b7afb6d1d71fa6831e43b701aea91
DevDeVice <sejkop@gmail.com>
2022-05-04 23:21:27 +0200

    22/23 zad

---
+++ 023/Program.cs
@@ -0,0 +1,41 @@
+﻿bool[] boolTable = new bool[28123];
+for (int i = 0; i < boolTable.Length; i++){ boolTable[i] = true; }
+
+int lastNumber = 28123;
+int[] arrayTwoAbundant = new int[7000];
+int arrayInt = 0;
+
+for (int i = 1; i < lastNumber; i++)
+{
+    var sumOfNumber = 0;
+    for (int j = 1; j <= i/2; j++) if (i % j == 0) sumOfNumber += j;
+    if (sumOfNumber > i)
+    {
+        arrayTwoAbundant[arrayInt] = i;
+        arrayInt++;
+    }
+}
+
+for(int i = 0; i < arrayTwoAbundant.Length; i++)
+{
+    if(arrayTwoAbundant[i] == 0) { break; }
+    for (int j = 0; j < arrayTwoAbundant.Length; j++)
+    {
+        if(arrayTwoAbundant[j] == 0) { break; }
+        if(arrayTwoAbundant[i] + arrayTwoAbundant[j] <= lastNumber)
+        {
+            boolTable[(arrayTwoAbundant[i] + arrayTwoAbundant[j])-1] = false;
+        }
+    }
+}
+
+int sum = 0;
+for (int i = 0; i < lastNumber ; i++)
+{
+    if (boolTable[i]) 
+    { 
+        sum += (i + 1);
+        Console.WriteLine(i+1);
+    }
+}
+Console.WriteLine(sum);
--- 22/Program.cs
+++ 22/Program.cs
@@ -5,7 +5,17 @@ var allNames = File.ReadAllLines(file)
     .SelectMany(l => l.Split(','))
     .Select(l => l.Trim('\"'))
     .ToList();
-foreach (var name in allNames)
+allNames.Sort();
+long totalScore = 0;
+for (int i = 0; i < allNames.Count; i++)
 {
-    Console.WriteLine($"Name: {name}");
+    var sumOfLetter = 0;
+    var name = allNames[i];
+    for (int j = 0; j < name.Length; j++)
+    {
+        sumOfLetter += (int)name[j] - 64;
+    }
+    totalScore += sumOfLetter * (i + 1);
+    Console.WriteLine($"Imie: {name} Score: {sumOfLetter*(i+1)}");
 }
+Console.WriteLine(totalScore);
